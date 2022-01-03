70a67fc91e757d1418e1fd4d8ba04bdf3b938e1c
Kunaal Kiran Kumar <55012954+Kunaalkk1@users.noreply.github.com>
2022-01-03 22:52:20 +0530

    Delete assets directory

--- assets/js/getIP.js
+++
@@ -1,4 +0,0 @@
-// Writes the user's public IP to a hidden field on the webpage, but it is recorded only when user clicks download
-$.getJSON("https://api.ipify.org?format=json", function(data) {
-      $("#my-ip").html(data.ip);
-  })

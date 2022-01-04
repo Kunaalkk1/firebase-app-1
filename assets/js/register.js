45d0e9eff1bf3a5e19e9db4234d29cd52e713c6d
Kunaal Kiran Kumar <55012954+Kunaalkk1@users.noreply.github.com>
2022-01-04 10:05:18 +0530

    . commit

--- assets/js/register.js
+++ assets/js/register.js
@@ -1,126 +1,58 @@
-document.getElementById("download-button").addEventListener("mouseover",ChangeToLight);
-document.getElementById("download-button").addEventListener("mouseout",ChangeToDark);
-document.getElementById("download-button").addEventListener("click",ClickEvent);
-document.getElementById("reset-button").addEventListener("click", resetForm);
-
-function validateEmail(emailID){
-    var re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return re.test(emailID);
-  }
-
-  function validateMobileNumber(number){
-  if(number.length==10){
-    var returnValue;
-  for(let i=0;i<=9;i++){
-    if((number[i].match(/^\d+/))){
-        returnValue = true;
-      }
-    else{
-        returnValue = false;
-        break;
-      }
-    }
-    return returnValue;
-  }
-  else{
-    return false;
-  }
-}
-function sendToDatabase(name,age,email,mobilenumber,university,ip){
-  try {
-        const docRef = await addDoc(collection(db, "userdata"), {
-        "name": name,
-        "age": age,
-        "email": email,
-        "mobileNumber": mobilenumber,
-        "university":university,
-        "ipAddress":ip
-        });
-        window.location.replace("download.html");
-        } catch (e) {
-        console.error("Error adding document: ", e);
-        if(window.confirm("An error occured! Retry?")){
-          window.location.reload();
-        }
-        else{
-          window.location.replace("index.html");
-          }
-        }
-      }
-//This function will be called on clicking the Download button
-function authenticateToFirebase(){
-    var name=document.getElementById("name").value;
-    var age=document.getElementById("age").value;
-    var email=document.getElementById("email").value;
-    var mobilenumber=document.getElementById("mobile-number").value;
-    var university=document.getElementById("university").value;
-    var ip=document.getElementById("my-ip").textContent;
-    var check=document.getElementById("terms-of-use").checked;
-    if (age.length==0){
-      age="18+"; //As per the terms of use, age should be 18 and above, by default.
-    if(name.length==0 || email.length==0 || mobilenumber.length==0){
-        window.alert("Required Fields Missing!");
-    } else{
-    if(validateEmail(email)==false || validateMobileNumber(mobilenumber)==false){
-        window.alert("Invalid Email or mobile number!");
-    }
-      else{
-        if(check==false){
-          alert("Please accept the terms and conditions before you proceed!")
-        }
-        else{
-          // Print data logs to console
-          console.log("Name: ", name);
-          console.log("Age: ", age);
-          console.log("Email: ", email);
-          console.log("Mobile Number: ", mobilenumber);
-          console.log("University: ", university);
-          console.log("Public IP Address: ", ip);
-          console.log("Checkbox: ", check);
-
-          // Insert data into Firestore Database here
-          sendToDatabase(name,age,email,mobilenumber,university,ip);
-            }
-          }
-        }
-      }
-    }
 function ChangeToLight(){
-    document.getElementById("android-icon").src="assets/images/android-icon-hover.png";
-    document.getElementById("ios-icon").src="assets/images/ios-icon-hover.png";
-
+  document.getElementById("android-icon").src="assets/images/android-icon-hover.png";
+  document.getElementById("ios-icon").src="assets/images/ios-icon-hover.png";
 }
+
 function ChangeToDark(){
-    document.getElementById("android-icon").src="assets/images/android-icon.png";
-    document.getElementById("ios-icon").src="assets/images/ios-icon.png";
-
-}
-function ClickEvent(){
-    authenticateToFirebase();
+  document.getElementById("android-icon").src="assets/images/android-icon.png";
+  document.getElementById("ios-icon").src="assets/images/ios-icon.png";
 }
 function resetForm(){
-    document.getElementById("registration-form").reset();
+  document.getElementById("registration-form").reset();
+}
+// Dismissal of events like opening developer tools or source code using shortcut keys
+document.onkeydown = function(e) {
+  if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
+    alert("This function is not allowed!");
+    return false;
+
+  }
+  if(e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
+    alert("This function is not allowed!");
+    return false;
+
+  }
+  if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
+    alert("This function is not allowed!");
+    return false;
+
+  }
+  if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
+    alert("This function is not allowed!");
+     return false;  
+  }
 }
 
-document.onkeydown = function(e) {
-    if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
-      alert("This function is not allowed!");
-      return false;
-  
+function validateEmail(emailID){
+  var re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return re.test(emailID);
+}
+
+function validateMobileNumber(number){
+if(number.length==10){
+  var returnValue;
+for(let i=0;i<=9;i++){
+  if((number[i].match(/^\d+/))){
+      returnValue = true;
     }
-    if(e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
-      alert("This function is not allowed!");
-      return false;
-  
-    }
-    if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
-      alert("This function is not allowed!");
-      return false;
-  
-    }
-    if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
-      alert("This function is not allowed!");
-       return false;
-       
+  else{
+      returnValue = false;
+      break;
     }
   }
+  return returnValue;
+}
+else{
+  return false;
+  }
+}
